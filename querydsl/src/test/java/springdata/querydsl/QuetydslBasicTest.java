c5cf22c9959f9a7181a535da1f144ceab428f9cd
d483ulert <tottenham3@naver.com>
2022-12-11 21:39:10 +0900

    검색 조건 쿼리

--- querydsl/src/test/java/springdata/querydsl/QuetydslBasicTest.java
+++ querydsl/src/test/java/springdata/querydsl/QuetydslBasicTest.java
@@ -80,7 +80,7 @@ public class QuetydslBasicTest {
                 .selectFrom(member)
                 .where(
                         member.username.eq("member1"),
-                        (member.age.eq(10))
+                        member.age.eq(10)
                 )
                 .fetchOne();
 
