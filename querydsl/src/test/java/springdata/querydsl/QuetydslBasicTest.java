3b61b49606883ae3722e28b6c7fa674457b3f301
d483ulert <tottenham3@naver.com>
2022-12-11 21:19:02 +0900

    시작 - JPQL vs Querydsl

---
+++ querydsl/src/test/java/springdata/querydsl/QuetydslBasicTest.java
@@ -0,0 +1,64 @@
+package springdata.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import springdata.querydsl.entity.Member;
+import springdata.querydsl.entity.QMember;
+import springdata.querydsl.entity.Team;
+import javax.persistence.EntityManager;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class QuetydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+    JPAQueryFactory queryFactory; //동시성 문제 없음. 멀티쓰레드에서 아무문제없음. 트랜잭션 단위에 따라 사용됨
+
+    @BeforeEach
+    public void before(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1  = new Member("member1",10,teamA);
+        Member member2  = new Member("member2",20,teamA);
+        Member member3  = new Member("member3",30,teamB);
+        Member member4  = new Member("member4",40,teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL(){
+        //member을 찾아라  jpql
+        Member findMember = em.createQuery("select m from Member m where m.username =: username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void startQuerydsl(){
+        QMember m = new QMember("m");
+        Member findmember = queryFactory.select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findmember.getUsername()).isEqualTo("member1");
+
+    }
+}
