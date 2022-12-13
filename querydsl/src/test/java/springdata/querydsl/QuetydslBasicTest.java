e65f29074dfc09d694920d21c0b892aa2e725784
d483ulert <tottenham3@naver.com>
2022-12-13 23:08:10 +0900

    조인 - 기본 조인

--- querydsl/src/test/java/springdata/querydsl/QuetydslBasicTest.java
+++ querydsl/src/test/java/springdata/querydsl/QuetydslBasicTest.java
@@ -239,4 +239,42 @@ public class QuetydslBasicTest {
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
 
+    /*
+    * 팀 A에 소속된 모든 회원
+    * */
+    @Test
+    public void join(){
+        //연관관계가 없어도 조인할 수 있음.
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1","member2");
+
+    }
+
+    /**
+     * 세타조인 (연관관계가 없는 조인)
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void theta_join(){
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA","teamB");
+    }
 }
