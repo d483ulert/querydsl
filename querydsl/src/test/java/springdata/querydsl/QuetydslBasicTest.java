94d92fa438c6caa2d88506128ee2650c58d9d980
d483ulert <tottenham3@naver.com>
2022-12-13 22:28:06 +0900

    정렬

--- querydsl/src/test/java/springdata/querydsl/QuetydslBasicTest.java
+++ querydsl/src/test/java/springdata/querydsl/QuetydslBasicTest.java
@@ -7,14 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import querydsl.entity.Member;
-import querydsl.entity.Team;
-
+import springdata.querydsl.entity.Member;
+import springdata.querydsl.entity.Team;
 import javax.persistence.EntityManager;
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static querydsl.entity.QMember.member;
+import static springdata.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -42,6 +40,9 @@ public class QuetydslBasicTest {
         em.persist(member2);
         em.persist(member3);
         em.persist(member4);
+
+        em.flush();
+        em.clear();
     }
 
     @Test
@@ -120,7 +121,31 @@ public class QuetydslBasicTest {
         long total = queryFactory
                 .selectFrom(member)
                 .fetchCount();
+    }
+    /*
+    * 회원 정렬 순서
+    * 1.회원 나이 내림차순(dssc)
+    * 2.회원 이름 올림차순(asc)
+    * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+    * */
+    @Test
+    public void sort(){
+        em.persist(new Member(null,100));
+        em.persist(new Member("member6",100));
+        em.persist(new Member("member7",100));
 
+        List<Member> result = queryFactory.selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
 
     }
 }
